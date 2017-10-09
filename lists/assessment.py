0e07116e7e976916c4f7b968e0b2d4271792940b
marie <fshbon677@gmail.com>
2017-10-09 01:07:23 +0000

    List assessment completed, all tests passed.

--- lists/assessment.py
+++ lists/assessment.py
@@ -19,7 +19,7 @@ def print_indices(items):
 
 def words_in_common(words1, words2):
     """Find words in common.
-    
+
     Given 2 lists of words, words1 and words2, return the words that are in
     common between the two, sorted alphabetically.
     """
@@ -47,39 +47,29 @@ def words_in_common(words1, words2):
 
 def every_other_item(items):
     """Return every other item in `items`, starting at first item.
-    For example::
-       >>> every_other_item(['a', 'b', 'c', 'd', 'e', 'f'])
-       ['a', 'c', 'e']
-       >>> every_other_item(["pickle", "pickle", "juice", "pickle", "juice", "pop"])
-       ['pickle', 'juice', 'juice']
-       >>> every_other_item(
-       ...   ["you", "z", "are", "z", "good", "z", "at", "x", "code"]
-       ... )
-       ['you', 'are', 'good', 'at', 'code']
+
+    Takes a single argument of type list, returns a list slice.
     """
 
-    return ['the wrong thing']
+    # Uses list slicing to skip every other item by having a step of 2.
+    return items[::2]
 
 
 def smallest_n_items(items, n):
     """Return the `n` smallest integers in list, in descending order.
-    You can assume that `n` will be less than the length of the list.
-    For example::
-    >>> smallest_n_items([2, 6006, 700, 42, 6, 59], 3)
-    [42, 6, 2]
 
-    It should work when `n` is 0::
-    >>> smallest_n_items([3, 4, 5], 0)
-    []
-
-    If there are duplicates in the list, they should be counted
-    separately::
-    >>> smallest_n_items([3, 1, 3, 2, 1, 1], 2)
-    [1, 1]
-
+    Takes two arguments, a list and a non-negative digit. The list is sorted and then sliced
+    so that only the smallest n items are returned in reverse order.
     """
 
-    return []
+    # Items is sorted in place.
+    # n of zero is treated differently to avoid unintended splicing.
+    # Starting from the nth item, the list is retured in reverse.
+    items = sorted(items)
+    if n != 0:
+        return items[n-1::-1]
+    else:
+        return []
 
 
 #####################################################################
